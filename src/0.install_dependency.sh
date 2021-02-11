d15b0027f3e2801aa7bd49c5f9a701a3c1952b76
ShaopengLiu1 <sml6467@psu.edu>
2021-02-11 14:58:27 -0500

    v1.0 generate random samples

--- src/0.install_dependency.sh
+++ src/0.install_dependency.sh
@@ -21,7 +21,9 @@ bash ${pipe_path}/CMash_prepare_environment.sh
 mkdir -p GenBank_db
 cd GenBank_db
 wget -O NCBI_GenBank_bacteria_assembly_summary.txt  ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt
-
+### note: this filter doesn't distinguish strain-level infor
+### when multiple hits found, I usually use the 1st one (which is newer)
+awk -F '\t'  '{if($12=="Complete Genome" && $11=="latest") print $6"\t"$8"\t"$20}' NCBI_GenBank_bacteria_assembly_summary.txt > NCBI_GenBank_download_link.txt
 
 
 
