580af69deead71fadbe2b52d63f056f0d0450132
ShaopengLiu1 <sml6467@psu.edu>
2021-02-10 22:53:16 -0500

    add src folder to gitignore

--- src/0.install_dependency.sh
+++ src/0.install_dependency.sh
@@ -9,6 +9,18 @@ mkdir -p conda_env
 mkdir -p git_repo
 
 
+# install CAMISIM env
+bash ${pipe_path}/CAMISIM_prepare_environment.sh
+
+
+# install CMash env
+bash ${pipe_path}/CMash_prepare_environment.sh
+
+
+# download NCBI GenBank bacteria database
+mkdir -p GenBank_db
+cd GenBank_db
+wget -O NCBI_GenBank_bacteria_assembly_summary.txt  ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt
 
 
 
